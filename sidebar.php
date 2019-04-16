f55ec21a2f8d836048480c6a1c3cdf95bbe5fbcb
Branko Jovanovic <branko.j.98@gmail.com>
2019-04-16 19:43:41 +0200

    Sedma verzija

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,25 @@
+<?php
+    $sql = "SELECT posts.id, posts.title, posts.created_at, posts.author, posts.body
+    FROM posts ORDER BY created_at DESC 
+    LIMIT 5";
+
+    $statement = $connection->prepare($sql);
+
+    $statement->execute();
+
+    $statement->setFetchMode(PDO::FETCH_ASSOC);
+
+    $posts = $statement->fetchAll();
+?>
+
 <aside class="col-sm-3 ml-sm-auto blog-sidebar">
             <div class="sidebar-module sidebar-module-inset">
-                <h4>About</h4>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+                <h4>Latest posts</h4>
+                <ul>
+                    <?php foreach($posts as $post){ ;?>
+                        <a href="single-post.php?post_id=<?php echo($post['id']) ?>"><p><?php echo ($post['title']); ?><p></a>
+                    <?php } ;?>
+                </ul>
             </div>
             <div class="sidebar-module">
                 <h4>Archives</h4>
