c843627a15db7117f94d8fbb3cc6339a0435aef6
Branko Jovanovic <branko.j.98@gmail.com>
2019-04-17 20:54:55 +0200

    14 verzija

---
+++ create-post.php
@@ -0,0 +1,37 @@
+<?php
+
+
+    include("db.php");
+
+    if(!empty($_POST['author'])){
+        $author = $_POST['author'];
+    } else{
+        header("Location: http://localhost:8000/create.php?required=false");
+        exit;
+    }
+
+    if(!empty($_POST['title'])){
+        $title = $_POST['title'];
+    } else {
+        header("Location: http://localhost:8000/create.php?required=false");
+        exit;
+    } 
+    if(!empty($_POST['body'])){
+        $body = $_POST['body'];
+    } else {
+        header("Location: http://localhost:8000/create.php?required=false");
+        exit;
+    } 
+        
+    $date = date('Y-m-d H:i:s');
+
+        $sqlCreate = "INSERT INTO posts (title, body, author, created_at) VALUES ('{$title}', '{$body}', '{$author}', '{$date}');";
+
+        $statementCreate = $connection->prepare($sqlCreate);
+
+        $statementCreate->execute();
+
+        $statementCreate->setFetchMode(PDO::FETCH_ASSOC);
+
+    header("Location: http://localhost:8000/index.php");
+?>
